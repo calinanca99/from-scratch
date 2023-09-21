418c1f44ee7dce2eaec007a990d6be5fbd826f67
calinanca99 <calinanca99@gmail.com>
2023-09-21 09:16:24 +0200

    Define

--- compression/run-length-encoding/rust/src/main.rs
+++ compression/run-length-encoding/rust/src/main.rs
@@ -1,3 +1,50 @@
+fn compress(input: &[u8]) -> Vec<u8> {
+    let mut i = 0;
+    let len = input.len();
+
+    let mut counter = 1;
+    let mut curr = input[0];
+
+    let mut res = vec![];
+
+    loop {
+        if i == (len - 1) {
+            // When reaching the end of the stream push to the buffer
+            // the number of occurrences and the current values of the
+            // latest identical elements.
+            res.push(counter);
+            res.push(curr);
+            break;
+        }
+
+        let next = input[i + 1];
+        if curr == next {
+            curr = input[i + 1];
+            counter += 1;
+
+            i += 1;
+        } else {
+            // When reaching the end of a series of identical elements
+            // push to the buffer the number of occurrences and the current
+            // value. Besides that, update the current value and reset the
+            // counter.
+            res.push(counter);
+            res.push(curr);
+
+            curr = input[i + 1];
+            counter = 1;
+
+            i += 1;
+        }
+    }
+
+    res
+}
+
 fn main() {
-    println!("Hello, world!");
+    let data = [0, 0, 0, 0, 1, 1, 1, 0, 0];
+
+    println!("Before compression: {:?}", data);
+    let compressed_data = compress(&data);
+    println!("After compression: {:?}", compressed_data);
 }
