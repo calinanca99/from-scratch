7bacb525cb3fb8a5e089d562ce9d56b269b3957e
calinanca99 <calinanca99@gmail.com>
2023-09-24 00:23:01 +0200

    Refactor

--- networking/http-server/rust/src/main.rs
+++ networking/http-server/rust/src/main.rs
@@ -1,6 +1,6 @@
 use std::{
     io::{Read, Write},
-    net::TcpListener,
+    net::{TcpListener, TcpStream},
 };
 
 fn main() {
@@ -11,33 +11,50 @@ fn main() {
 
     for stream in server.incoming() {
         match stream {
-            Ok(mut conn) => {
-                let mut buf = [0; 1024];
-
-                let bytes_read = conn.read(&mut buf).expect("Cannot read from socket");
-
-                let request = String::from_utf8_lossy(&buf[..bytes_read]);
-                let res = if let Some(start_line) = request.lines().nth(0) {
-                    println!("start-line: {start_line}");
-                    if let Some(route) = start_line.split_whitespace().nth(1) {
-                        println!("route: {route}");
-                        match route {
-                            "/" | "/health_check" => "HTTP/1.1 200 OK",
-                            _ => "HTTP/1.1 404 Not Found",
-                        }
-                    } else {
-                        "HTTP/1.1 400 Bad Request"
-                    }
-                } else {
-                    "HTTP/1.1 400 Bad Request"
-                };
-
-                conn.write_all(res.as_bytes().as_ref())
-                    .expect("Cannot write to socket");
-            }
+            Ok(mut conn) => handle_conn(&mut conn),
             Err(_) => {
                 println!("Connection failed")
             }
         }
     }
+}
+
+fn handle_conn(conn: &mut TcpStream) {
+    let mut buf = [0; 1024];
+    let bytes_read = conn.read(&mut buf).expect("Cannot read from socket");
+    let request = String::from_utf8(buf[..bytes_read].to_vec()).expect("Cannot parse request");
+
+    let res = match handle_request(&request) {
+        Ok(res) => res,
+        Err(_) => "HTTP/1.1 400 Bad Request",
+    };
+
+    conn.write_all(res.as_bytes().as_ref())
+        .expect("Cannot write to socket");
+}
+
+fn handle_request(req: &str) -> Result<&'static str, String> {
+    let start_line = req.lines().nth(0).ok_or("bad-request".to_string())?;
+    let route = start_line
+        .split_whitespace()
+        .nth(1)
+        .ok_or("bad-request".to_string())?;
+
+    match route {
+        "/" => root(),
+        "/health_check" => health_check(),
+        _ => fallback(),
+    }
+}
+
+fn root() -> Result<&'static str, String> {
+    Ok("HTTP/1.1 200 OK")
+}
+
+fn health_check() -> Result<&'static str, String> {
+    Ok("HTTP/1.1 200 OK")
+}
+
+fn fallback() -> Result<&'static str, String> {
+    Ok("HTTP/1.1 404 Not Found")
 }
