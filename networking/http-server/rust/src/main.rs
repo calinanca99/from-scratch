1757c0eee700970292201d31cd64c88e49a56828
calinanca99 <calinanca99@gmail.com>
2023-09-23 17:15:12 +0200

    Return 200 for "/" and "/health_check"

--- networking/http-server/rust/src/main.rs
+++ networking/http-server/rust/src/main.rs
@@ -1,3 +1,43 @@
+use std::{
+    io::{Read, Write},
+    net::TcpListener,
+};
+
 fn main() {
-    println!("Hello, world!");
+    let addr = "http://127.0.0.1:4000";
+    let server = TcpListener::bind("127.0.0.1:4000").expect("Cannot bind socket");
+
+    println!("Listening on: {}", addr);
+
+    for stream in server.incoming() {
+        match stream {
+            Ok(mut conn) => {
+                let mut buf = [0; 1024];
+
+                let bytes_read = conn.read(&mut buf).expect("Cannot read from socket");
+
+                let request = String::from_utf8_lossy(&buf[..bytes_read]);
+                let res = if let Some(start_line) = request.lines().nth(0) {
+                    println!("start-line: {start_line}");
+                    if let Some(route) = start_line.split_whitespace().nth(1) {
+                        println!("route: {route}");
+                        match route {
+                            "/" | "/health_check" => "HTTP/1.1 200 OK",
+                            _ => "HTTP/1.1 404 Not Found",
+                        }
+                    } else {
+                        "HTTP/1.1 400 Bad Request"
+                    }
+                } else {
+                    "HTTP/1.1 400 Bad Request"
+                };
+
+                conn.write_all(res.as_bytes().as_ref())
+                    .expect("Cannot write to socket");
+            }
+            Err(_) => {
+                println!("Connection failed")
+            }
+        }
+    }
 }
