e675021406b9f1453171910dc05b3126f96aaf3f
calinanca99 <calinanca99@gmail.com>
2023-09-28 08:30:38 +0200

    init shell

---
+++ cli-tools/shell/rust/target/debug/deps/shell-373b0b21d46bf7fc.d
@@ -0,0 +1,5 @@
+/home/calin/src/projects/from-scratch/cli-tools/shell/rust/target/debug/deps/shell-373b0b21d46bf7fc.rmeta: src/main.rs
+
+/home/calin/src/projects/from-scratch/cli-tools/shell/rust/target/debug/deps/shell-373b0b21d46bf7fc.d: src/main.rs
+
+src/main.rs:
