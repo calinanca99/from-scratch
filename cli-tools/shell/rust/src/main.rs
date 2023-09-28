6c5b7c58d451ac3ecbf39a457b955b38e602de3a
calinanca99 <calinanca99@gmail.com>
2023-09-28 17:17:24 +0200

    accept flags

--- cli-tools/shell/rust/src/main.rs
+++ cli-tools/shell/rust/src/main.rs
@@ -3,6 +3,35 @@ use std::{
     process::Command,
 };
 
+struct Program {
+    exec_path: String,
+    args: Vec<String>,
+}
+
+impl Program {
+    fn new(s: String) -> Self {
+        let v = s
+            .split_whitespace()
+            .map(|s| s.to_string())
+            .collect::<Vec<String>>();
+
+        if v.is_empty() {
+            panic!("Command cannot contain only whitespace");
+        }
+
+        let args = if v.len() == 1 {
+            vec![]
+        } else {
+            v[1..].to_vec()
+        };
+
+        Self {
+            exec_path: v[0].to_string(),
+            args,
+        }
+    }
+}
+
 fn main() {
     println!("\n####### shell-rs #######\n");
 
@@ -13,14 +42,13 @@ fn main() {
         let mut buf = String::new();
         let br = io::stdin().read_line(&mut buf).expect("Cannot read stdin");
 
-        // TODO: Handle if input is just whitespace
         if br == 0 {
             break;
         }
 
-        // Don't read the trailing '\n' from `buf`
-        let cmd = &buf[..br - 1];
-        let res = match Command::new(cmd).output() {
+        let program = Program::new(buf);
+
+        let res = match Command::new(program.exec_path).args(program.args).output() {
             Ok(output) => match String::from_utf8(output.stdout) {
                 Ok(s) => s,
                 Err(e) => e.to_string(),
