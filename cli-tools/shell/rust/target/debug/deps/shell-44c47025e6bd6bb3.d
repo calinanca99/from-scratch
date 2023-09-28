098d2c110d08926c34dc17bf42388dd2f7e94ebb
calinanca99 <calinanca99@gmail.com>
2023-09-28 08:31:49 +0200

    Add .gitignore

--- cli-tools/shell/rust/target/debug/deps/shell-44c47025e6bd6bb3.d
+++
@@ -1,5 +0,0 @@
-/home/calin/src/projects/from-scratch/cli-tools/shell/rust/target/debug/deps/shell-44c47025e6bd6bb3.rmeta: src/main.rs
-
-/home/calin/src/projects/from-scratch/cli-tools/shell/rust/target/debug/deps/shell-44c47025e6bd6bb3.d: src/main.rs
-
-src/main.rs:
