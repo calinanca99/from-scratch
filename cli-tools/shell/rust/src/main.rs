97f5269c9f456d95b683040d98906f3d02685683
calinanca99 <calinanca99@gmail.com>
2023-09-28 16:36:32 +0200

    accept commands and echo them

--- cli-tools/shell/rust/src/main.rs
+++ cli-tools/shell/rust/src/main.rs
@@ -1,3 +1,19 @@
+use std::io::{self, Write};
+
 fn main() {
-    println!("Hello, world!");
+    println!("\n####### shell-rs #######\n");
+
+    loop {
+        print!(">>> ");
+        io::stdout().flush().unwrap();
+
+        let mut buf = String::new();
+        let br = io::stdin().read_line(&mut buf).unwrap();
+
+        println!("{buf}");
+
+        if br == 0 {
+            break;
+        }
+    }
 }
